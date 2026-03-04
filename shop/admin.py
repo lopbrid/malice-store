e8416edfed99aeb815fe2d25530303a1daabf961
pitiirns <wendellcapareda770@gmail.com>
2026-03-04 13:26:01 +0800

    Fix admin.py: define inlines before admin classes

--- shop/admin.py
+++ shop/admin.py
@@ -1,29 +1,40 @@
 from django.contrib import admin
 from .models import Category, Product, ProductVariant, CartItem, Order, OrderItem
-from django.utils.text import slugify
 
+# -----------------------------
+# Product Variant Inline
+# -----------------------------
 class ProductVariantInline(admin.TabularInline):
     model = ProductVariant
     extra = 1
     fields = ['size', 'stock', 'sku']
-    readonly_fields = ['sku']  # Make SKU read-only (auto-generated)
+    readonly_fields = ['sku']
 
-    def get_formset(self, request, obj=None, **kwargs):
-        # Auto-generate SKU on save
-        formset = super().get_formset(request, obj, **kwargs)
-        return formset
+# -----------------------------
+# Order Item Inline (must be before OrderAdmin)
+# -----------------------------
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+    readonly_fields = ['product', 'variant', 'price', 'quantity']
+    extra = 0
 
+# -----------------------------
+# Category Admin
+# -----------------------------
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ['name', 'slug']
-    prepopulated_fields = {'slug': ('name',)}  # Auto-generate slug from name
+    prepopulated_fields = {'slug': ('name',)}
 
+# -----------------------------
+# Product Admin
+# -----------------------------
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ['name', 'slug', 'price', 'get_total_stock', 'available', 'created']
     list_filter = ['available', 'created', 'updated']
     list_editable = ['price', 'available']
-    prepopulated_fields = {'slug': ('name',)}  # THIS auto-generates slug from name!
+    prepopulated_fields = {'slug': ('name',)}
     inlines = [ProductVariantInline]
     search_fields = ['name', 'description']
     date_hierarchy = 'created'
@@ -32,48 +43,49 @@ class ProductAdmin(admin.ModelAdmin):
         return obj.get_total_stock()
     get_total_stock.short_description = 'Total Stock'
 
+# -----------------------------
+# Product Variant Admin
+# -----------------------------
 @admin.register(ProductVariant)
 class ProductVariantAdmin(admin.ModelAdmin):
     list_display = ['product', 'size', 'stock', 'sku']
     list_filter = ['size']
     search_fields = ['product__name', 'sku']
     list_editable = ['stock']
-    readonly_fields = ['sku']  # Make SKU read-only in admin
+    readonly_fields = ['sku']
 
+# -----------------------------
+# Order Admin
+# -----------------------------
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
+    inlines = [OrderItemInline]  # references OrderItemInline defined above
     list_display = ['id', 'first_name', 'last_name', 'email', 'phone', 'payment_method', 'status', 'total', 'created']
     list_filter = ['status', 'payment_method', 'created']
     search_fields = ['first_name', 'last_name', 'email', 'phone', 'address']
-    inlines = [OrderItemInline]
-    readonly_fields = ['created', 'updated', 'confirmed_at', 'sku']
-    
+    readonly_fields = ['created', 'updated', 'confirmed_at']
+
     fieldsets = (
-        ('Customer Info', {
-            'fields': ('first_name', 'last_name', 'email', 'phone')
-        }),
-        ('Shipping Address', {
-            'fields': ('address', 'city', 'postal_code', 'latitude', 'longitude')
-        }),
-        ('Payment', {
-            'fields': ('payment_method', 'total')
-        }),
-        ('Status', {
-            'fields': ('status', 'created', 'updated', 'confirmed_at')
-        }),
+        ('Customer Info', {'fields': ('first_name', 'last_name', 'email', 'phone')}),
+        ('Shipping Address', {'fields': ('address', 'city', 'postal_code', 'latitude', 'longitude')}),
+        ('Payment', {'fields': ('payment_method', 'total')}),
+        ('Status', {'fields': ('status', 'created', 'updated', 'confirmed_at')}),
     )
-    
+
     actions = ['confirm_orders', 'mark_shipped']
-    
+
     def confirm_orders(self, request, queryset):
         from django.utils import timezone
         queryset.update(status='confirmed', confirmed_at=timezone.now())
     confirm_orders.short_description = "Mark selected orders as confirmed"
-    
+
     def mark_shipped(self, request, queryset):
         queryset.update(status='shipped')
     mark_shipped.short_description = "Mark selected orders as shipped"
 
+# -----------------------------
+# Cart Item Admin
+# -----------------------------
 @admin.register(CartItem)
 class CartItemAdmin(admin.ModelAdmin):
     list_display = ['user', 'product', 'variant', 'quantity', 'created']
