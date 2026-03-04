901940a1c14ea636ceb4675e02e13c571bc457cb
pitiirns <wendellcapareda770@gmail.com>
2026-03-04 10:46:54 +0800

    Initial commit

--- config/wsgi.py
+++ config/wsgi.py
@@ -1,16 +1,12 @@
-"""
-WSGI config for config project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
-"""
-
 import os
+import sys
 
-from django.core.wsgi import get_wsgi_application
+# Add your project directory to the sys.path
+path = '/home/yourusername/malice_ecommproj'
+if path not in sys.path:
+    sys.path.insert(0, path)
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
+from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
