42639ed78ef3f8cb3e9f41f556d00d28f85dd231
pitiirns <wendellcapareda770@gmail.com>
2026-03-04 16:54:03 +0800

    Remove broken Context monkey patch

--- shop/apps.py
+++ shop/apps.py
@@ -1,14 +1,5 @@
-# config/__init__.py
-import copy
-from django.template import context
+from django.apps import AppConfig
 
-original_copy = context.Context.__copy__
-
-def patched_copy(self):
-    try:
-        return original_copy(self)
-    except AttributeError:
-        from django.template.context import Context
-        return Context(dict=self.flatten())
-
-context.Context.__copy__ = patched_copy
+class ShopConfig(AppConfig):
+    default_auto_field = 'django.db.models.AutoField'
+    name = 'shop'
