076d925c016c5789ec9458cf2ec6ad97fddd0ee6
pitiirns <wendellcapareda770@gmail.com>
2026-03-09 22:59:25 +0800

    Fix DEBUG setting to use environment variable

--- config/settings.py
+++ config/settings.py
@@ -17,7 +17,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY', default='django-insecure-%7_au68r_#c7b%n$#2$u^fr&7hb-dwvc7jw6ll+ak_64k#qu%1')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default=False, cast=bool)
+DEBUG = True
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='*', cast=Csv())
 
